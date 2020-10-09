52a6f02804260bd9452045b98e8412e3583ccefe
Arjun Aloshious <arjunaloshious@gmail.com>
2020-10-09 13:24:07 +0530

    Update Success.java

--- Form Authentication/classes/Success.java
+++ Form Authentication/classes/Success.java
@@ -3,12 +3,13 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class Success extends HttpServlet {
-  protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+  protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException 
+  {
     res.setContentType("text/html");  
     PrintWriter out = res.getWriter();
-	String name=req.getParameter("username");  
+    String name=req.getParameter("username");  
     String pass=req.getParameter("password");
-	out.print("<html><head><title>Successful Authentication</title></head><body><center><h1>Authentication Status : Successful</h1><h3><br>Name: "+name+"<br><br>Password: "+pass+"</h3><center></body></html>"); 	
-	out.close();  
+    out.print("<html><head><title>Successful Authentication</title></head><body><center><h1>Authentication Status : Successful</h1><h3><br>Name: "+name+"<br><br>Password: "+pass+"</h3><center></body></html>"); 	
+    out.close();  
   }
 }
