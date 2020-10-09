1c489dfc62d19844cb7e01704bca793da851fea8
Arjun Aloshious <arjunaloshious@gmail.com>
2020-10-09 13:26:08 +0530

    Update Fail.java

--- Form Authentication/classes/Fail.java
+++ Form Authentication/classes/Fail.java
@@ -3,7 +3,8 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class Fail extends HttpServlet {
-  protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+  protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException 
+  {
     res.setContentType("text/html");  
     PrintWriter out = res.getWriter();
     out.print("<html><head><title>Authentication Failed</title></head><body><center><h1>Authentication Status : Failed</h1><center></body></html>"); 	
