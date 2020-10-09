412921d664eae9f21e11b8442959c41ad171f0ae
Arjun Aloshious <arjunaloshious@gmail.com>
2020-10-09 13:25:19 +0530

    Update LoginServlet.java

--- Form Authentication/classes/LoginServlet.java
+++ Form Authentication/classes/LoginServlet.java
@@ -8,15 +8,13 @@ public class LoginServlet extends HttpServlet {
     String name=req.getParameter("username");  
     String pass=req.getParameter("password");          
     res.setContentType("text/html");  
-    
-	if(!((name == null) || name.isEmpty()) && !((pass == null) || pass.isEmpty()))
-	{
-		RequestDispatcher rd = req.getRequestDispatcher("AppServlet");
-		rd.forward(req,res);
-	}
-	
-	PrintWriter out = res.getWriter();  
+    if(!((name == null) || name.isEmpty()) && !((pass == null) || pass.isEmpty()))
+    {
+	RequestDispatcher rd = req.getRequestDispatcher("AppServlet");
+	rd.forward(req,res);
+    }
+    PrintWriter out = res.getWriter();  
     out.println("<html><head><title>Welcome</title></head><body><h1>Login</h1><br><form>Name:<input type = \"text\" name = \"username\"><br><br>Password:<input type = \"text\" name = \"password\"><br><br><input type = \"submit\" value = \"Submit\"></body></html>");
-	out.close();
+    out.close();
   }
 }
